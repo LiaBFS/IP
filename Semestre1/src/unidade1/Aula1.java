a741a4ffcccab6337b307f5683d1818697ec89f8
Lia Broring Fontes Schramm <lbfschramm@furb.br>
2026-02-23 19:58:13 -0300

    Sysout's

--- Semestre1/src/unidade1/Aula1.java
+++ Semestre1/src/unidade1/Aula1.java
@@ -2,4 +2,15 @@ package unidade1;
 
 public class Aula1 {
 
+    public static void main(String[] args) {
+        
+        System.out.println( "oi");
+
+        System.out.println("Texto\ndividido");
+
+        System.out.print("Texto ");
+        System.out.println("sem ln");
+
+    }
+
 }
