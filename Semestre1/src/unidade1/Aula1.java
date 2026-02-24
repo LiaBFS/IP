9422dafc6f83991291d5e5a726e78e22a3968059
Lia Broring Fontes Schramm <lbfschramm@furb.br>
2026-02-23 21:41:40 -0300

    Media

--- Semestre1/src/unidade1/Aula1.java
+++ Semestre1/src/unidade1/Aula1.java
@@ -1,8 +1,10 @@
 package unidade1;
 
+import java.util.Scanner;
+
 public class Aula1 {
 
-    public static void main(String[] args) {
+    /**public static void main(String[] args) {
         
         System.out.println( "oi");
 
@@ -10,6 +12,30 @@ public class Aula1 {
 
         System.out.print("Texto ");
         System.out.println("sem ln");
+
+    } **/
+
+        // MEDIA
+
+    public static void main(String[] args) {
+        
+        float n1;
+        float n2;
+        float n3;
+        float dv;
+
+        System.out.println("Digite 3 notas:");
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextFloat();
+        n2 = sc.nextFloat();
+        n3 = sc.nextFloat();
+
+        System.out.println("Escolha um divisor para média:");
+        dv = sc.nextFloat();
+
+        float media = (n1+n2+n3)/dv;
+
+        System.out.println("Media = "+ media);
 
     }
 
