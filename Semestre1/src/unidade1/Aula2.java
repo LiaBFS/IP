9407e2b81c1f25fe286065add6d2b735b841d702
Lia Broring Fontes Schramm <lbfschramm@furb.br>
2026-02-26 20:00:41 -0300

    Aula 2

---
+++ Semestre1/src/unidade1/Aula2.java
@@ -0,0 +1,60 @@
+package unidade1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aula2 {
+
+    //ÁREA
+
+    /**public static void main(String[] args) {
+        
+        float frente;
+        float lateral;
+
+        Scanner sc = new Scanner(System.in);
+        frente = sc.nextFloat();
+        lateral = sc.nextFloat();
+
+
+        float area = frente * lateral;
+
+        System.out.println("Área = "+area);
+
+        sc.close();
+
+    }**/
+
+
+        //MÉDIA PONDERADA
+
+    public static void main(String[] args) {
+        
+        float n1;
+        float n2;
+        float pf;
+        float nEx[] = new float[6];
+        float me;
+        float media;
+
+        System.out.println("nota 1:");
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextFloat();
+
+        System.out.println("nota 2:");
+        n2 = sc.nextFloat();
+
+        System.out.println("nota projeto final:");
+        pf = sc.nextFloat();
+
+        
+        //for(int i; i<nEx)
+        
+        //System.out.println("nota do  exercicio "+ i+1+":");
+        //nEx[0] = sc.nextFloat();
+
+
+
+    }
+
+}
