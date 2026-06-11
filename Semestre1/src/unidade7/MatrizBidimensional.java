d91b9d46843a5e1adfe70cdc1a13ba3ac0d7e7bc
lia <liabroring@gmail.com>
2026-06-11 20:02:30 -0300

    dicas para o trabalho

--- Semestre1/src/unidade7/MatrizBidimensional.java
+++ Semestre1/src/unidade7/MatrizBidimensional.java
@@ -1,11 +1,32 @@
 package unidade7;
 
+//import java.util.Random;
+
 public class MatrizBidimensional {
 
     public static void main(String[] args) {
         
         char vetor[][] = new char[3][3];
+        //Random random = new Random();
+        //int linhaC = random.nextInt(3);
+        //int colunaC = random.nextInt(3);
+        
         vetor[0][0] = 'X';
+        vetor[0][1] = 'O';
+        vetor[0][2] = 'O';
+        vetor[1][0] = 'O';
+        vetor[1][1] = 'X';
+        vetor[1][2] = 'O';
+        vetor[2][0] = 'X';
+        vetor[2][1] = 'O';
+        vetor[2][2] = 'X';
+
+        for(int linha=0; linha<3; linha++){
+            for(int coluna=0; coluna<3; coluna++){
+                System.out.println(vetor[linha][coluna]+" ");
+            }
+            System.out.println(" ");
+        }
 
     }
 
